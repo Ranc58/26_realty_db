20196124486ecf0845cc9e35ab22da7d248098ab
Ranc58 <rancvova@gmail.com>
2017-08-01 16:28:07 +0300

    init upload info to sqlite

---
+++ db_operations.py
@@ -0,0 +1,43 @@
+import os
+import requests
+from db_init import db
+from models import Flat
+
+DB_NAME = 'flats_content.db'
+JSON_URL = 'https://devman.org/assets/ads.json'
+
+
+def create_db():
+    if not os.path.exists(DB_NAME):
+        db.create_all()
+
+
+def get_flat_content():
+    json_content = requests.get(JSON_URL).json()
+    return json_content
+
+
+def add_flat_content_to_db(json_content):
+    for flat in json_content:
+        if Flat.ob
+        session = db.session
+        flat_content = Flat(settlement=flat['settlement'],
+                            under_construction=flat['under_construction'],
+                            description=flat['description'],
+                            price=flat['price'],
+                            oblast_district=flat['oblast_district'],
+                            living_area=flat['living_area'],
+                            has_balcony=flat['has_balcony'],
+                            address=flat['address'],
+                            construction_year=flat['construction_year'],
+                            rooms_number=flat['rooms_number'],
+                            premise_area=flat['premise_area'],
+                            id=flat['id'])
+        session.add(flat_content)
+        session.commit()
+
+
+if __name__ == '__main__':
+    create_db()
+    json_content = get_flat_content()
+    add_flat_content_to_db(json_content)
---
+++ models.py
@@ -0,0 +1,20 @@
+from db_init import db, app
+
+
+class Flat(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    settlement = db.Column(db.String(64), index=True)
+    under_construction = db.Column(db.Boolean)
+    description = db.Column(db.Text)
+    price = db.Column(db.Integer)
+    oblast_district = db.Column(db.String(128))
+    living_area = db.Column(db.Float)
+    has_balcony = db.Column(db.Boolean)
+    address = db.Column(db.String(128))
+    construction_year = db.Column(db.Integer)
+    rooms_number = db.Column(db.Integer)
+    premise_area = db.Column(db.Float)
+
+    def __repr__(self):
+        return '%r' % (self.id)
+
